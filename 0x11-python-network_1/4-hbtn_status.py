0567c8e53891ba5cf5f3237c2cb342c25199c7f0
abdessamadsalim <samimalim1991@gmail.com>
2024-04-26 05:01:25 -0700

    task 4 upd

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import requests
 
-"""
-get https://alx-intranet.hbtn.io/status
-"""
-
-from requests import get
-
-def get_alx_intranet(url='https://alx-intranet.hbtn.io/status'):
-    
-    stat = get(url)
-    print("Body response:")
-    print("\t- type: {}".format(str(stat).__class__))
-    print("\t- content: {}".format(stat.text))
 
 if __name__ == "__main__":
-    get_alx_intranet()
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
