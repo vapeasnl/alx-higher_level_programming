079941ad70c143d0e2d498d129e3ad213cf7102c
abdessamadsalim <samimalim1991@gmail.com>
2024-04-26 04:32:54 -0700

    upd

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """displays value of the X-Request-Id in response"""
+
 from urllib import request, error
 from sys import argv
+
+
 def request_header_property(url: str) -> str: 
     try:
         with request.urlopen(url) as rep:
             return rep.info()['X-Request-Id']
     except error.URLError as er:
         return er.reason
+
+
 if __name__ == "__main__":
     print(request_header_property(argv[1]))
 
