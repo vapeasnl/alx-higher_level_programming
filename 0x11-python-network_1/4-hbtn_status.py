f919c3dfaec5041781a2effa215651c83f70032e
abdessamadsalim <samimalim1991@gmail.com>
2024-04-26 01:01:49 -0700

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+get https://alx-intranet.hbtn.io/status using
+requests and print the response to stdout
+"""
+
+from requests import get
+
+def get_alx_intranet(url='https://alx-intranet.hbtn.io/status'):
+    """
+    Send a GET request to the url
+    and print the reponse
+    """
+    res = get(url)
+    print("Body response:")
+    print("\t- type: {}".format(str(res).__class__))
+    print("\t- content: {}".format(res.text))
+
+if __name__ == "__main__":
+    get_alx_intranet()
