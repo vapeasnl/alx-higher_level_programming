1d5811af79f197d94b52f355090e83140d16c67b
abdessamadsalim <samimalim1991@gmail.com>
2024-05-21 02:25:08 -0700

    upd

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const API_URL = process.argv[2];
-let movieCount = 0;
+let movieCompt = 0;
 
 request(API_URL, (error, response, body) => {
   if (error) {
@@ -12,9 +12,9 @@ request(API_URL, (error, response, body) => {
     const filmChars = results[filmIdx].characters;
     for (const charIndex in filmChars) {
       if (filmChars[charIndex].includes('18')) {
-        movieCount += 1;
+        movieCompt += 1;
       }
     }
   }
-  console.log(movieCount);
+  console.log(movieCompt);
 });
