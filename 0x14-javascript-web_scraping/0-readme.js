5d21f5cc0686da0d233e3b202e2c8de197202c2d
abdessamadsalim <samimalim1991@gmail.com>
2024-05-21 01:32:27 -0700

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
+
+const fileName = process.argv[2];
 const fs = require('fs');
 
-process.argv[0] = 'node';
-function readFile (filePath) {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-  });
-}
-
-if (process.argv.length === 3) {
-  const filePath = process.argv[2];
-  readFile(filePath);
+fs.readFile(fileName, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(data);
+});
